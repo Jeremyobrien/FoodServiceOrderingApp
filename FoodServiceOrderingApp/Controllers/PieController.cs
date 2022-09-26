56092e539e7b6e7808dd8c1e4bad194c2900c96d
jeremyobrien <jjob1391@gmail.com>
2022-09-26 12:10:25 -0700

    Add Pie View and PieListViewModel

---
+++ FoodServiceOrderingApp/Controllers/PieController.cs
@@ -0,0 +1,25 @@
+﻿using FoodServiceOrderingApp.Models;
+using FoodServiceOrderingApp.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FoodServiceOrderingApp.Controllers
+{
+    public class PieController : Controller
+    {
+        private readonly IPieRepository _pieRepository;
+        private readonly ICategoryRepository _categoryRepository;
+
+        public PieController(IPieRepository pieRepository, ICategoryRepository categoryRepository)
+        {
+            _pieRepository = pieRepository;
+            _categoryRepository = categoryRepository;
+        }
+
+        public IActionResult List()
+        {
+            PieListViewModel piesListViewModel = new PieListViewModel
+                (_pieRepository.AllPies, "Cheese cakes");
+            return View(piesListViewModel);  
+        }
+    }
+}
