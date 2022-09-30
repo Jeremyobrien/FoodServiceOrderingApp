bf5e556d76d842f083c59507bb5322e3917bf9df
jeremyobrien <jjob1391@gmail.com>
2022-09-30 12:11:49 -0700

    Add shopping cart summary

---
+++ FoodServiceOrderingApp/Components/ShoppingCartSummary.cs
@@ -0,0 +1,27 @@
+﻿using FoodServiceOrderingApp.Models;
+using FoodServiceOrderingApp.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FoodServiceOrderingApp.Components
+{
+    public class ShoppingCartSummary: ViewComponent
+    {
+        private readonly IShoppingCart _shoppingCart;
+
+        public ShoppingCartSummary(IShoppingCart shoppingCart)
+        {
+            _shoppingCart = shoppingCart;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var items = _shoppingCart.GetShoppingCartItems();
+            _shoppingCart.ShoppingCartItems = items;
+
+            var shoppingCartViewModel = new ShoppingCartViewModel(_shoppingCart, 
+                _shoppingCart.GetShoppingCartTotal());
+
+            return View(shoppingCartViewModel);
+        }
+    }
+}
