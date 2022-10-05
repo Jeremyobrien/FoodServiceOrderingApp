ca349aeed15495b1cc8a04c2a494500b90d87e97
jeremyobrien <jjob1391@gmail.com>
2022-10-05 14:05:09 -0700

    Create search page with jquery/ajax functionality

--- FoodServiceOrderingApp/Controllers/PieController.cs
+++ FoodServiceOrderingApp/Controllers/PieController.cs
@@ -49,5 +49,10 @@ namespace FoodServiceOrderingApp.Controllers
                 return NotFound();
             return View(pie);
         }
+
+        public IActionResult Search()
+        {
+            return View();
+        }
     }
 }
