a443feb81918213c4a95aa7d21b187963a82b15e
jeremyobrien <jjob1391@gmail.com>
2022-10-06 13:00:08 -0700

    Add authorization to checkout

--- FoodServiceOrderingApp/Controllers/OrderController.cs
+++ FoodServiceOrderingApp/Controllers/OrderController.cs
@@ -1,8 +1,10 @@
 ﻿using FoodServiceOrderingApp.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FoodServiceOrderingApp.Controllers
 {
+    [Authorize]
     public class OrderController : Controller
     {
         private readonly IOrderRepository _orderRepository;
